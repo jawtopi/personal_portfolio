bbc771cc5b125c37f76ebdc956852f8dcc6451a0
jawtopi <jasunlee24@gmail.com>
2025-01-06 21:40:40 +1300

    reset

--- react-portfolio-2025/vite.config.js
+++ react-portfolio-2025/vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-portfolio-2025/",
   server: {
     port: 3000,
   },
