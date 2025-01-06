707c933fabe0c1afb9bad23a18101ee1ca9b9d1f
jawtopi <jasunlee24@gmail.com>
2025-01-07 01:28:03 +1300

    make hamburger menu white in dark mode

--- src/pages/components/SideBar.jsx
+++ src/pages/components/SideBar.jsx
@@ -44,7 +44,7 @@ const Sidebar = ({ isDarkMode, refs }) => {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke="currentColor"
+          stroke={isDarkMode ? "white" : "black"}
           strokeWidth={2}
         >
           <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16m-7 6h7" />
